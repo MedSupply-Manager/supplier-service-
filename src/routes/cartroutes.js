5916477f670f23193f38a5a8f77a899820740dd7
Abd Elhadi <mohamezarifi@gmail.com>
2025-11-14 18:30:32 -0500

    Update cartroutes.js

--- src/routes/cartroutes.js
+++ src/routes/cartroutes.js
@@ -3,22 +3,24 @@ const router = express.Router();
 const Cart = require("../models/cart");
 
 router.get("/", async (req, res) => {
-    try {
-      const cartItems = await Cart.findAll();
-      res.json(cartItems);
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: "Internal server error" });
-    }
-  });
+  try {
+    const cartItems = await Cart.findAll();
+    res.json(cartItems);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 router.post("/", async (req, res) => {
-    try {
-      const { product_id, quantity } = req.body;
-      const newCartItem = await Cart.create({ product_id, quantity });
-      res.status(201).json(newCartItem);
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: "Internal server error" });
-    }
-  });
+  try {
+    const { product_id, quantity } = req.body;
+    const newCartItem = await Cart.create({ product_id, quantity });
+    res.status(201).json(newCartItem);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+module.exports = router;
